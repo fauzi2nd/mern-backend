15aeab6cc18f720b84ed3eee4c5feb90e79fc436
Fauzi Pranata <spacetone2nd@gmail.com>
2022-11-30 00:19:04 +0700

    fix api

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ app.use(
     cors({
         credentials: true,
         origin: "*",
+        methods: ["GET", "POST"]
     })
 );
 
-app.use('/', (req,res) => {
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('/', (req, res) => {
     res.send("hello world");
 });
 
